938c851f86cdde24ff5937e7063691820972d4e6
OladanZaka Channel <oladanzaka@gmail.com>
2026-03-01 11:21:02 +0800

    rev6

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+window.addEventListener("load", function(){
+document.getElementById("preloader").style.display="none";
+});
+
 function openInvite(){
 document.getElementById("envelope").style.display="none";
 document.getElementById("content").style.display="block";
@@ -22,6 +26,7 @@ document.getElementById("guestName").textContent=guest;
 
 // Countdown
 const countDate=new Date("May 06, 2026 10:00:00").getTime();
+
 setInterval(()=>{
 const now=new Date().getTime();
 const distance=countDate-now;
@@ -31,6 +36,22 @@ const h=Math.floor((distance%(1000*60*60*24))/(1000*60*60));
 const m=Math.floor((distance%(1000*60*60))/(1000*60));
 const s=Math.floor((distance%(1000*60))/1000);
 
-document.getElementById("countdown").innerHTML=
-d+" Hari "+h+" Jam "+m+" Menit "+s+" Detik";
+document.getElementById("days").innerHTML=d;
+document.getElementById("hours").innerHTML=h;
+document.getElementById("minutes").innerHTML=m;
+document.getElementById("seconds").innerHTML=s;
+
 },1000);
+
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+loop:true,
+pagination: {
+el: ".swiper-pagination",
+clickable: true,
+},
+autoplay:{
+delay:3000,
+disableOnInteraction:false,
+},
+});
